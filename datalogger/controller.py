d46996cfd3f9ed873055b07f815c5ade24e5fb3c
RunnanLi <lirunnan90@gmail.com>
2022-05-01 16:15:37 +1000

    updated controller setters

--- datalogger/controller.py
+++ datalogger/controller.py
@@ -20,46 +20,58 @@ class Controller(object):
     def stop_pwm(self):
         raise NotImplementedError
 
+    def __del__(self):
+        self.stop_pwm()
+
 
 class NMosPWM(Controller):
     def __init__(self, pin_no, frequency=25_000, pinout_type=BOARD):
         if pin_no not in HW_PWM_MAP[pinout_type].keys():
             raise ValueError(f'RPI pin {pin_no} not support hardware pwm')
-        self.freq = frequency
-        self.duty_cycle = None
+        self.__frequency = frequency
+        self.__duty_cycle = None
         self.is_stopped = True
         self.pwm = HardwarePWM(pwm_channel=HW_PWM_MAP[pinout_type][pin_no], hz=self.freq)
 
     @property
     def frequency(self):
-        return self.freq
+        return self.__frequency
 
     @frequency.setter
     def frequency(self, value):
-        self.freq = value
+        self.set_frequency(value)
+
+    @property
+    def duty_cycle(self):
+        return self.__duty_cycle
+
+    @duty_cycle.setter
+    def duty_cycle(self, value):
+        self.set_duty_cycle(value)
+
+    @duty_cycle.setter
+    def duty_cycle(self, value):
+        self.__duty_cycle = value
 
     def set_frequency(self, frequency: int):
         if frequency <= 0:
             raise ValueError('frequency need to be positive non-zero int')
         self.pwm.change_frequency(frequency)
-        self.freq = frequency
+        self.__frequency = frequency
 
     def set_duty_cycle(self, duty_cycle: int):
         if not 0 <= duty_cycle <= 100:
             raise ValueError('Duty cycle can only be non negative int from 0 to 100')
         self.pwm.change_duty_cycle(duty_cycle)
-        self.duty_cycle = duty_cycle
+        self.__duty_cycle = duty_cycle
 
     def start_pwm(self, duty_cycle: int):
         if not 0 <= duty_cycle <= 100:
             raise ValueError('Duty cycle can only be non negative int from 0 to 100')
         self.pwm.start(duty_cycle)
-        self.duty_cycle = duty_cycle
+        self.__duty_cycle = duty_cycle
         self.is_stopped = False
 
     def stop_pwm(self):
         self.pwm.stop()
         self.is_stopped = True
-
-    def __del__(self):
-        self.stop_pwm()
