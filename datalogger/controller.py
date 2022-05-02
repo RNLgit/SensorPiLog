2ab16aa1b11a6481e1f067d66fb5c82b6cf5924c
Runnan Li <runnanl@blackmagicdesign.com>
2022-05-02 11:14:04 +1000

    service and argparse added

--- datalogger/controller.py
+++ datalogger/controller.py
@@ -71,3 +71,31 @@ class NMosPWM(Controller):
     def stop_pwm(self):
         self.pwm.stop()
         self.is_stopped = True
+
+
+if __name__ == '__main__':
+    import argparse
+    import time
+    type_map = {'board': BOARD, 'bcm': BCM}
+
+    parser = argparse.ArgumentParser(description='Smart control RPI CPU core temperature set point with PWM fan')
+    parser.add_argument('-p', '--pin', dest='pin', default=12, type=int,
+                        help='The PWM pin number (RPI GPIO board type) controlling the fan')
+    parser.add_argument('-b', '--board-type', dest='board_type', default='board', type=str,
+                        help=f'RPI pin board type. options: f{list(type_map.keys())}')
+    parser.add_argument('-t', '--temperature_min', dest='temperature_min', default=55, type=int,
+                        help='Temperature set point (to turn fan on)')
+    parser.add_argument('-d', '--duty-cycle', dest='duty_cycle', default=20, type=int,
+                        help='Minimum duty cycle can turn fan on from stall')
+    parser.add_argument('-f', '--frequency', dest='frequency', default=25_000, type=int,
+                        help='PWM frequency. Ideally set frequency outside audible range')
+    parser.add_argument('-m', '--temperature_max', dest='temperature_max', default=80, type=int,
+                        help='Temperature point that turn fan to full speed')
+    parser.add_argument('-t', '--time', dest='time', default=10, type=int,
+                        help='After temperature reached min set point, delay time (s) before fan can turn back on')
+
+    ops = parser.parse_args()
+
+    fan_h = NMosPWM(ops.pin, ops.frequency, type_map[ops.board_type])
+    fan_h.start_pwm(ops.duty_cycle)
+    time.sleep(3)
